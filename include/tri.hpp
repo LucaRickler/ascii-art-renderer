e5af494da98c7aff6f9ddf846767be8fdb2dea71
Luca Rickler <luca.rickler@gmail.com>
2018-09-28 14:17:04 +0200

    Vertex structs

---
+++ include/tri.hpp
@@ -0,0 +1,15 @@
+#ifndef TRI_HPP
+#define TRI_HPP
+
+#include <vertex.hpp>
+
+struct Tri {
+  Vertex *v0, *v1, *v2;
+  Tri(Vertex* v0, Vertex* v1, Vertex* v2) {
+    this->v0 = v0;
+    this->v1 = v1;
+    this->v2 = v2;
+  }
+};
+
+#endif
---
+++ include/vertex.hpp
@@ -0,0 +1,12 @@
+#ifndef VERTEX_HPP
+#define VERTEX_HPP
+
+#include <glm/glm.hpp>
+
+struct Vertex {
+  glm::vec4 pos;
+  glm::vec4 color;
+  glm::vec4 normal;
+};
+
+#endif
